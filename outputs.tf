c568c8cfd1670b2b1d71dc61385e1e3e60b201d1
Grzegorz Kocur <grzegorz.kocur@softwaremill.com>
2022-06-01 17:17:00 +0200

    Refactor output names

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,12 @@ output "gke_cluster_name" {
   value = module.kubernetes_engine.name
   description = "Cluster name"
 }
-output "endpoint" {
+output "gke_endpoint" {
   value       = module.kubernetes_engine.endpoint
   sensitive   = true
   description = "The kubernetes endpoint"
 }
-output "ca_certificate" {
+output "gke_ca_certificate" {
   value       = module.kubernetes_engine.ca_certificate
   sensitive   = true
   description = "The kubernetes CA certificate"
