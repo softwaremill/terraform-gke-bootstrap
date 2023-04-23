fb56d139c2a6a98e2db9ced17cf611c244986a7d
Grzegorz Kocur <grzegorz.kocur@softwaremill.com>
2023-04-23 21:04:20 +0200

    Fix formatting

--- locals.tf
+++ locals.tf
@@ -11,6 +11,6 @@ locals {
   location               = var.regional ? var.region : var.zones.0
   node_pool_names        = [for np in toset(var.node_pools) : np.name]
   node_pools             = zipmap(local.node_pool_names, tolist(toset(var.node_pools)))
-  node_locations         = var.regional ? (length(var.zones) !=0 ? var.zones : null) : slice(var.zones, 1, length(var.zones))
+  node_locations         = var.regional ? (length(var.zones) != 0 ? var.zones : null) : slice(var.zones, 1, length(var.zones))
   node_pool_oauth_scopes = { for key, value in var.additional_node_pool_oauth_scopes : key => distinct(concat(value, var.default_node_pools_oauth_scopes)) }
 }
