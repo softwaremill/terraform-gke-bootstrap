f3987a59297c897abffb6fc8480a0f17945525d3
Grzegorz Kocur <grzegorz.kocur@softwaremill.com>
2024-06-07 13:57:48 +0200

    Fix formating

--- examples/autopilot/main.tf
+++ examples/autopilot/main.tf
@@ -1,13 +1,13 @@
 module "gke" {
-  source           = "../../"
-  create_project   = false
-  project_id       = var.project_id
-  enable_autopilot = true
+  source                      = "../../"
+  create_project              = false
+  project_id                  = var.project_id
+  enable_autopilot            = true
   disable_services_on_destroy = false
-  k8s_network_base = "10.100.0.0/16"
-  platform_name    = var.platform_name
-  region           = "europe-central2"
-  subnet_network   = "10.1.0.0/20"
-  regional         = true
-  node_pools       = {}
+  k8s_network_base            = "10.100.0.0/16"
+  platform_name               = var.platform_name
+  region                      = "europe-central2"
+  subnet_network              = "10.1.0.0/20"
+  regional                    = true
+  node_pools                  = {}
 }
